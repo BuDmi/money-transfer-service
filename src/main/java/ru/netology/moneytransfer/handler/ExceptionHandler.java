8a42c0798374a2ad857240325f8156f7adb06a08
Dmitriy Bubyr <d.bubyr@lancktele.com>
2023-02-02 14:12:01 +0300

    Fix after code review

--- src/main/java/ru/netology/moneytransfer/handler/ExceptionHandler.java
+++ src/main/java/ru/netology/moneytransfer/handler/ExceptionHandler.java
@@ -2,7 +2,6 @@ package ru.netology.moneytransfer.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.netology.moneytransfer.exception.ErrorConfirmation;
 import ru.netology.moneytransfer.exception.ErrorInputData;
@@ -10,21 +9,21 @@ import ru.netology.moneytransfer.exception.ErrorTransfer;
 import ru.netology.moneytransfer.model.ErrorResponse;
 
 @RestControllerAdvice
-public class ExceptionHandlerAdvice {
+public class ExceptionHandler {
 
-    @ExceptionHandler(ErrorInputData.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(ErrorInputData.class)
     public ResponseEntity<ErrorResponse> eidHandler(ErrorInputData e) {
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), httpStatus.value()), httpStatus);
     }
 
-    @ExceptionHandler(ErrorTransfer.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(ErrorTransfer.class)
     public ResponseEntity<ErrorResponse> etHandler(ErrorTransfer e) {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), httpStatus.value()), httpStatus);
     }
 
-    @ExceptionHandler(ErrorConfirmation.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(ErrorConfirmation.class)
     public ResponseEntity<ErrorResponse> ecHandler(ErrorConfirmation e) {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), httpStatus.value()), httpStatus);
